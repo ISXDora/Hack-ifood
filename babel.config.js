c79a75f2e95f571265c138a7b40906d82a0ad3bd
ISXDora <isinhasena@gmail.com>
2022-02-20 11:55:18 -0300

    alteração Carlos

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "presets": ["@babel/preset-env", "@babel/preset-flow"],
-    "plugins": [
-      "@babel/proposal-class-properties", "@babel/plugin-transform-flow-strip-types", "tensorflow"
-    ]
-  }
